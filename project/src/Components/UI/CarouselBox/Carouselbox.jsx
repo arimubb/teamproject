6bf1bda481c68f265160c9b45197346b6ecdd50c
arimubb <alimoloo@mail.ru>
2023-03-14 17:59:06 +0600

    new file:   project/src/Components/UI/CarouselBox/Carouselbox.jsx

---
+++ project/src/Components/UI/CarouselBox/Carouselbox.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Carousel from 'react-bootstrap/'
+
+export default class Carouselbox extends Component {
+  render() {
+    return (
+      <div>Carouselbox</div>
+    )
+  }
+}
