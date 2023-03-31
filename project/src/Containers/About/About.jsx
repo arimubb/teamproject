3ae8207d6aa3549fe5550162be0f99c7472489d8
misterluk <belekbajabdyraev0@gmail.com>
2023-03-31 15:04:54 +0600

    new file:   project/public/iconsBelya/Vector (1).png 	new file:   project/public/iconsBelya/vector (2).png 	renamed:    project/public/imgBelya/img.png -> project/public/imgBelya/hareImg.png 	modified:   project/src/Containers/About/About.jsx 	modified:   project/src/Containers/About/About.module.css

--- project/src/Containers/About/About.jsx
+++ project/src/Containers/About/About.jsx
@@ -7,12 +7,17 @@ import serviceCD1 from '../../../public/imgBelya/services-image-1.png'
 import serviceCD2 from '../../../public/imgBelya/services-image-2.png'
 import serviceCD3 from '../../../public/imgBelya/services-image-3.png'
 import serviceCD4 from '../../../public/imgBelya/services-image-4.png'
-import rewiew from '../../../public/imgBelya/image 29.png'
 import rewiew2 from '../../../public/imgBelya/about-studio-image.png'
 import ouractivity1 from '../../../public/imgBelya/activity-image-1.png'
 import ouractivity2 from '../../../public/imgBelya/activityimage2.png'
+import aboutCardApplication from '../../../public/imgBelya/hareImg.png'
+import ApplicationVector from '../../../public/iconsBelya/vector (2).png'
+
 
 const About = () => {
+  const retis = () =>{
+    alert('Заявка отправлена')
+  }
   return (
     <div className={styles.aboutGlavCon}>
       <div className={styles.aboutTitle}>
@@ -33,7 +38,7 @@ const About = () => {
          <div className={styles.aboutDescTitle}>
           <h2 className={styles.descTitle}>Создаем красивые стены</h2>
           <div className={styles.aboutDescText}>
-          <p className={styles.descText}>Но базовый вектор развития влечет за собой процесс внедрения и
+          <p className={styles.descText1}>Но базовый вектор развития влечет за собой процесс внедрения и
           модернизации соответствующих условий активизации. В целом, конечно, сплочённость команды
           профессионалов требует определения и уточнения поставленных обществом задач.</p>
           <p className={styles.descText}>Разнообразный и богатый опыт говорит нам, что внедрение современных
@@ -56,10 +61,14 @@ const About = () => {
           <Card img={serviceCD4} title={"Обновить стены без ремонта"} text={"С помощью опыта нашей команды преобразить любые помещения не составит труда вне зависимости от масштаба или сложности задачи."}/>
         </div>
 
-    <div className={styles.aboutRewiew}>
+    {/* <div className={styles.aboutRewiew}>
    <div className={styles.aboutRewtext}><h2 className={styles.rewTitle}>Мы используем разные техники оформления</h2>
    <p className={styles.rewText}>Разнообразный и богатый опыт говорит нам, что внедрение современных методик представляет собой интересный эксперимент проверки кластеризации усилий. Предварительные выводы неутешительны: базовый вектор развития создаёт предпосылки для дальнейших направлений развития. Не следует, однако, забывать, что граница обучения кадров прекрасно подходит для реализации своевременного выполнения сверхзадачи. Разнообразный и богатый опыт говорит нам, что высокое качество позиционных исследований, а также свежий взгляд на привычные вещи — безусловно открывает новые горизонты для позиций, занимаемых участниками в отношении поставленных задач.</p></div>
-   <img className={styles.rewVideo} src={rewiew} alt="" />
+   <div className={styles.aboutRewImgCon}>
+   <button  className={styles.RewBtn}><a href="https://youtu.be/P5AupXIhGHk"><img src="../../public/iconsBelya/Vector (1).png" alt="" /></a></button>
+   </div>
+  
+   
     </div>
     <div className={styles.aboutRewiew2}>
       <img className={styles.rewVideo2} src={rewiew2} alt="" />
@@ -73,13 +82,41 @@ const About = () => {
     <div className={styles.OurScrollimg}>
       <img src={ouractivity1} alt="" />
       <img src={ouractivity2} alt="" />
+      <img src={ouractivity1} alt="" />
+      <img src={ouractivity2} alt="" />
     </div>    
     </div>
   </div>
-  
-    </div>
 
-    // </div>
+  
+  <div className={styles.block2}>
+      <div className={styles.blk4}>
+        <p className={styles.zayavka}>Оставьте заявку</p>
+        <p className={styles.zav}>Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время и ответит на все интересующие вопросы.</p>
+     <div className={styles.input}>
+      <div className="h">
+      <h3 className={styles.h3}>Ваше имя:</h3>
+      <input type="text" className={styles.inp1} placeholder='Иван' />
+      </div>
+      <div className={styles.m}>
+      <h3 className={styles.h3}>Номер телефона:</h3>
+      <input type="number" className={styles.inp1} placeholder='+7 (___) ___-__-__' />
+      </div>
+      </div>
+      <div className={styles.cov}>
+      <button onClick={retis} className={styles.btn}>Отправить заявку</button>
+      <p className={styles.sullka}>Нажимая «отправить», я даю согласие на <a href="#"> обработку персональных данных</a></p>
+      </div>
+     </div>
+     <div className={styles.image}>
+      <img className={styles.vec} src={ApplicationVector} alt="" />
+      <img className={styles.img3} src={aboutCardApplication} alt=""/>
+     </div>
+      </div> */}
+
+
+
+    </div>
   )
 }
 
