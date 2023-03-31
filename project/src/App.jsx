5fc527bd9f0636e92bd32b42d73a05b7d2cd6027
islamerkinbekov <janybekosmonbekuule@gmail.com>
2023-03-31 15:39:54 +1000

    new file:   project/public/img/Group 3 (1).png 	new file:   project/public/img/Vector (12).png 	new file:   project/public/img/Vector (14).png 	new file:   project/public/img/Vector (15).png 	new file:   project/public/img/bes.png 	new file:   project/public/img/besigns.png 	new file:   project/public/img/image 16.png 	new file:   project/public/img/image 17.png 	new file:   project/public/img/image 18 (1).png 	new file:   project/public/img/image 21 (1).png 	new file:   project/public/img/image 29.png 	new file:   project/public/img/image 30.png 	new file:   project/public/img/image 35 (1).png 	new file:   project/public/img/img (2).png 	modified:   project/src/App.jsx 	modified:   project/src/Containers/Contacts/Contacts.module.css 	modified:   project/src/Containers/Services/Service.jsx 	modified:   project/src/Containers/Services/Service.module.css 	new file:   project/src/Containers/Services/Service_detal.jsx 	new file:   project/src/Containers/Services/Service_detal.module.css

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -9,6 +9,7 @@ import Contacts from './Containers/Contacts/Contacts'
 import Main from './Containers/Main/Main'
 import Portfolio from './Containers/Portfolio/Portfolio'
 import Service from './Containers/Services/Service'
+import Service_detal from './Containers/Services/Service_detal'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
         <Route path="/blog" element={<Blog/>}/>
         <Route path="/portfolio" element={<Portfolio/>}/>
         <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="/service_detail" element={<Service_detal/>}/>
       </Routes>
     </div>
       
